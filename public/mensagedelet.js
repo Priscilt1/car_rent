c01009d733c4705c00e1f4103eebc7ea98224de4
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-01-26 09:07:26 -0300

    limpando arquivo

--- public/mensagedelet.js
+++
@@ -1,9 +0,0 @@
-const formDelete = document.querySelector("#form-delete")
-formDelete.addEventListener("submit", function(event){
-    const confirmation = confirm("Deseja deletar?")
-    if(!confirmation) {
-        event.preventDefault()
-    }
-})
-
-module.exports = mensagedelet.js
