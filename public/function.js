0958961896261f328504bee02a5dfe174204da60
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-01-27 14:32:34 -0300

    mascara de porcentagem

--- public/function.js
+++ public/function.js
@@ -1,25 +1,27 @@
-function printDouble(number, callback ){
-    return new Promise (resolve => {
-        setTimeout(
-            () => {
-              callback(number)
-              resolve(number)
-            }, 
-            Math.floor(Math.random() * 100) + 1
-          )
-    })
-  }
+// function printDouble(number, doubleNumber, callback){
+//     return new Promise (resolve => {
+//         setTimeout(
+//             () => {
+                
+//                 resolve(callback(number, doubleNumber))
+//             }, 
+//             Math.floor(Math.random() * 100) + 1
+//           )
+//     })
+//   }
 
-function myCallback (number) {
-    console.log(number *2)
-}
+// function myCallback (number, doubleNumber) {
+//     return (number *2) + doubleNumber
+// }
   
-async function printAll(){
-    await printDouble(5, myCallback) //10
-    await printDouble(10, myCallback) //20
-    await printDouble(22, myCallback) //44
-    await printDouble(1, myCallback) //2
-    await printDouble(89, myCallback) //178
-  }
+// async function printAll(){
+//     let result 
+//     result = await printDouble(5, 0, myCallback); // retorna 10
+//     console.log(result)
+//     result = await printDouble(12, result, myCallback); // retorna 34
+//     console.log(result)
+//     result = await printDouble(2, result, myCallback); // retorna 38
+//     console.log(result)
+// }
   
-  printAll()
+//   printAll()
--- public/mask.js
+++ public/mask.js
@@ -1,1 +1,9 @@
-const maskPercentage 
+function maskPercentage (number) {
+    return new Intl.NumberFormat('pt-BR',
+    { style:'percent',
+        minimumFractionDigits: 2, 
+        maximumFractionDigits: 4
+    }).format(number/100)
+}
+
+console.log(maskPercentage(25.1))
