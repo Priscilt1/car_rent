8be89673033a2ab13942b4436ce4e5edc97e5fc2
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-01-28 10:10:16 -0300

    mascara CPF

--- public/function.js
+++ public/function.js
@@ -1,27 +1,27 @@
-// function printDouble(number, doubleNumber, callback){
-//     return new Promise (resolve => {
-//         setTimeout(
-//             () => {
+function printDouble(number, doubleNumber, callback){
+    return new Promise (resolve => {
+        setTimeout(
+            () => {
                 
-//                 resolve(callback(number, doubleNumber))
-//             }, 
-//             Math.floor(Math.random() * 100) + 1
-//           )
-//     })
-//   }
+                resolve(callback(number, doubleNumber))
+            }, 
+            Math.floor(Math.random() * 100) + 1
+          )
+    })
+  }
 
-// function myCallback (number, doubleNumber) {
-//     return (number *2) + doubleNumber
-// }
+function myCallback (number, doubleNumber) {
+    return (number *2) + doubleNumber
+}
   
-// async function printAll(){
-//     let result 
-//     result = await printDouble(5, 0, myCallback); // retorna 10
-//     console.log(result)
-//     result = await printDouble(12, result, myCallback); // retorna 34
-//     console.log(result)
-//     result = await printDouble(2, result, myCallback); // retorna 38
-//     console.log(result)
-// }
+async function printAll(){
+    let result 
+    result = await printDouble(5, 0, myCallback); // retorna 10
+    console.log(result)
+    result = await printDouble(12, result, myCallback); // retorna 34
+    console.log(result)
+    result = await printDouble(2, result, myCallback); // retorna 38
+    console.log(result)
+}
   
-//   printAll()
+  printAll()
--- public/mask.js
+++ public/mask.js
@@ -7,3 +7,13 @@ function maskPercentage (number) {
 }
 
 console.log(maskPercentage(25.1))
+
+
+function maskCpf(event) {
+        setTimeout(function(){
+            let valueFormated = event.target.value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g,function( regex, argumet1, argumet2, argumet3, argumet4 ) {
+                return argumet1 + '.' + argumet2 + '.' + argumet3 + '-' + argumet4
+        })
+            event.target.value = valueFormated
+        }, 1)
+}
